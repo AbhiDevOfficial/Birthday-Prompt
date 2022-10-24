dccb9d5fff6a57c9d2428d4a12ef6f12dcb60708
ABHI <114732679+AbhiDevOfficial@users.noreply.github.com>
2022-10-24 14:07:28 +0530

    Main File

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+
+promptName = "What's your name? :)\n"
+wish = "Hey {} ! Happy Birthday! May god bless you <3\n"
+name = input(promptName)
+print(wish.format(name))
+time.sleep(2)
+print("Follow AbhiDevOfficial @ github for more! ;)")
+p = "POWERED"
+by = "BY"
+u = "@AbhiDevOfficial"
+for letter in p:
+    time.sleep(0.5)
+    print(letter)
+   
+print("\n\n")
+  
+for lete in by:
+    time.sleep(0.5)
+    print(lete)
+   
+print("\n\n")
+
+for us in u:
+    time.sleep(0.5)
+    print(us)
